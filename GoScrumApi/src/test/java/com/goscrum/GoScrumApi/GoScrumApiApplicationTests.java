1a7bf9b0aa9b61423e6703b00e983226cc80cbc1
mymai91 <my.maithi@gmail.com>
2024-06-23 21:50:43 +1200

    Init GoScrum project

---
+++ GoScrumApi/src/test/java/com/goscrum/GoScrumApi/GoScrumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goscrum.GoScrumApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoScrumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
