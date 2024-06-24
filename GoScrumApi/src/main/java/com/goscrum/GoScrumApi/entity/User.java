6c4b5ff47a0d767fd40ee32eba0cd9e37cb6aa90
mymai91 <my.maithi@gmail.com>
2024-06-24 22:06:14 +1200

    User entity

--- GoScrumApi/src/main/java/com/goscrum/GoScrumApi/entity/User.java
+++ GoScrumApi/src/main/java/com/goscrum/GoScrumApi/entity/User.java
@@ -1,2 +1,34 @@
-package com.GoScrum.GoScrumApi.entity;public class User {
+package com.GoScrum.GoScrumApi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String device_id;
+
+    @Column(nullable = false)
+    private String password;
+
 }
