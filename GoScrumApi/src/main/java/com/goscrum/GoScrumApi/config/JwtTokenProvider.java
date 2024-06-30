9a086558192830ad288a5709b6360fe3a909a3d7
mymai91 <my.maithi@gmail.com>
2024-06-30 15:46:08 +1200

    Create JwtTokenProvider

--- GoScrumApi/src/main/java/com/goscrum/GoScrumApi/config/JwtTokenProvider.java
+++ GoScrumApi/src/main/java/com/goscrum/GoScrumApi/config/JwtTokenProvider.java
@@ -13,71 +13,73 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+	@Value("${app.jwt-secret}")
+	private String jwtSecret;
 
-    @Value("${app.jwt-secret}")
-    private String jwtSecret;
+	@Value("${app.jwt-expiration-in-ms}")
+	private long jwtExpirationDate;
 
-    @Value("${app.jwt-expiration-in-ms}")
-    private long jwtExpirationDate;
+	// generate JWT token
+	public String generateToken(Authentication authentication) {
+		String username = authentication.getName();
 
-    // generate JWT token
-    public String generateToken(Authentication  authentication) {
-        String username = authentication.getName();
+		Date currentDate = new Date();
+		Date expirationDate = new Date(currentDate.getTime() + jwtExpirationDate);
 
-        Date currentDate = new Date();
-        Date expirationDate = new Date(currentDate.getTime() + jwtExpirationDate);
+		String token = Jwts.builder()
+				.setSubject(username)
+				.setIssuedAt(currentDate)
+				.setExpiration(expirationDate)
+				.signWith(key())
+				.compact();
 
-        String token = Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(currentDate)
-                .setExpiration(expirationDate)
-                .signWith(key())
-                .compact();
-
-        return token;
-    }
-
+		return token;
+	}
 
 
-    // get username from JWT token
-    public String getUsername(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(key())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        String username = claims.getSubject();
-        return username;
-    }
+	// get username from JWT token
+	public String getUsername(String token) {
+		Claims claims = Jwts.parser()
+				.setSigningKey(key())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		String username = claims.getSubject();
+		return username;
+	}
 
-    // validate JWT token
-    public boolean validateToken(String token) {
+	// validate JWT token
+	public boolean validateToken(String token) {
 
-        try {
-            Jwts.parser().verifyWith(key()).build().parseSignedClaims(token).getPayload();
-            return true;
-        } catch (JwtException ex) {
-            return false;
-        }
-            // } catch (MalformedJwtException ex) {
-            //     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
-            //     throw new Exception("Invalid JWT token");
-            // } catch (ExpiredJwtException ex) {
-            //  throw new Exception("Expired JWT token");
-            //     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
-            // } catch (UnsupportedJwtException ex) {
-            // throw new Exception("Unsupported JWT token");
-            //     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
-            //
-            // } catch (IllegalArgumentException ex) {
-            //     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claims string is empty.");
-            // throw new Exception("JWT claims string is empty.");
-            // }
-    }
+		try {
+			Jwts.parser()
+					.verifyWith(key())
+					.build()
+					.parseSignedClaims(token)
+					.getPayload();
+			return true;
+		} catch (JwtException ex) {
+			return false;
+		}
+		// } catch (MalformedJwtException ex) {
+		//     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Invalid JWT token");
+		//     throw new Exception("Invalid JWT token");
+		// } catch (ExpiredJwtException ex) {
+		//  throw new Exception("Expired JWT token");
+		//     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Expired JWT token");
+		// } catch (UnsupportedJwtException ex) {
+		// throw new Exception("Unsupported JWT token");
+		//     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Unsupported JWT token");
+		//
+		// } catch (IllegalArgumentException ex) {
+		//     // throw new BlogAPIException(HttpStatus.BAD_REQUEST, "JWT claims string is empty.");
+		// throw new Exception("JWT claims string is empty.");
+		// }
+	}
 
-    private SecretKey key() {
-        return Keys.hmacShaKeyFor(
-                Decoders.BASE64.decode(jwtSecret)
-        );
-    }
+	private SecretKey key() {
+		return Keys.hmacShaKeyFor(
+				Decoders.BASE64.decode(jwtSecret)
+		);
+	}
 }
