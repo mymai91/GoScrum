67d296eb9dc631426361b8c5f33ee586b3e08f76
mymai91 <my.maithi@gmail.com>
2024-07-01 22:09:47 +1200

    Login Api return jwt token

--- GoScrumApi/src/main/java/com/goscrum/GoScrumApi/controller/AuthController.java
+++ GoScrumApi/src/main/java/com/goscrum/GoScrumApi/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.GoScrum.GoScrumApi.controller;
 
+import com.GoScrum.GoScrumApi.payload.JWTAuthResponse;
+import com.GoScrum.GoScrumApi.payload.LoginDto;
 import com.GoScrum.GoScrumApi.payload.RegisterDto;
 import com.GoScrum.GoScrumApi.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    private AuthService authService;
+	private AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
 
-    @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        String response = authService.register(registerDto);
+	@PostMapping("/register")
+	public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+		String response = authService.register(registerDto);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto) {
+		String token = authService.login(loginDto);
+
+		JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+		jwtAuthResponse.setToken(token);
+
+		return new ResponseEntity<>(jwtAuthResponse, HttpStatus.OK);
+	}
 }
